c131edeaaa1d109f9c654bdbe42026f7e32d517b
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2022-12-04 11:48:27 -0600

    Add project files.

---
+++ Data/Entities/Patient.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+
+namespace HandOffApiCli.Data.Entities
+{
+    public class Patient
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public string patient_fname { get; set; }
+        [MaxLength(50)]
+        public string patient_lname { get; set; }
+        [MaxLength(50)]
+        public string? patient_city { get; set; }
+        [MaxLength(25)]
+        public string? patient_phone { get; set; }
+        public int? patient_vist_id { get; set; }
+        public int? patient_primary_care_id { get; set; }
+    }
+  
+}
---
+++ Data/HandOffContext.cs
@@ -0,0 +1,18 @@
+﻿using HandOffApiCli.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HandOffApiCli.Data
+{
+    public class HandOffContext : DbContext
+    {
+        public HandOffContext(DbContextOptions<HandOffContext> options) : base(options){ }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("server=127.0.0.1;uid=root;pwd=Star5820!;database=test");
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+    }
+}
