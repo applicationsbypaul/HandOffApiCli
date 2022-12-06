e8d4aed5a2a2a02505846278294f9fbc03b23b67
PaulFord <applicationsbypaul@gmail.com>
2022-12-05 23:50:54 -0600

    updating where it failed to accept changes

---
+++ Data/Entities/WorkGroup.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HandOffApiCli.Data.Entities
+{
+    public class WorkGroup
+    {
+        [Key] 
+        public int id { get; set; }
+        public int wg_employee_id { get; set; }
+    }
+}
--- Migrations/20221206050856_testDBModels.Designer.cs
+++ Migrations/20221206050856_testDBModels.Designer.cs
@@ -4,6 +4,7 @@ using HandOffApiCli.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HandOffApiCli.Migrations
 {
     [DbContext(typeof(HandOffContext))]
-    partial class HandOffContextModelSnapshot : ModelSnapshot
+    [Migration("20221206050856_testDBModels")]
+    partial class testDBModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
