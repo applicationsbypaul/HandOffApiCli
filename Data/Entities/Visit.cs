fb998d41f6ced419df856292657719429fac18f4
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2022-12-05 21:53:29 -0600

    Creating the visit class

---
+++ Data/Entities/Visit.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HandOffApiCli.Data.Entities
+{
+    public class Visit
+    {
+        [Key] public int id { get; set; }
+        public DateTime visit_date { get; set; }
+        [Required]
+        public int visit_handoff_id { get; set; }
+        public int work_group_id { get; set; }
+
+    }
+}
